43c0bb9a529d8fd363154903631b45e520d190cf
shannon horn <shannonahorn@icloud.com>
2021-08-27 17:32:03 +0000

    wip

---
+++ controls/newsletter.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Newsletter.module.css";
+
+const Newsletter = () => {
+  return <section>newsletter</section>;
+};
+
+export default Newsletter;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../controls/header";
 import Marquee from "../controls/marquee";
+import Newsletter from "../controls/newsletter";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
@@ -8,7 +9,11 @@ const Home = () => {
     <div className={styles.container}>
       <Head>
         <title>Allied Jiu Jitsu in Peoria and Cottonwood AZ</title>
-        <script defer data-domain="alliedjiujitsu.com" src="https://plausible.io/js/plausible.js"></script>
+        <script
+          defer
+          data-domain="alliedjiujitsu.com"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
         <meta
           name="description"
           content="Allied Jiu Jitsu is a premier American Jiu Jitsu academy with locations in Peoria and Cottonwood, Arizona. We also offer fitness and striking classes for both adults and youth and concealed carry tactical self-defense seminars. Call us today or drop by to check things out FREE!"
@@ -22,6 +27,7 @@ const Home = () => {
       <main className={styles.main}>
         <Header />
         <Marquee />
+        <Newsletter />
       </main>
     </div>
   );
