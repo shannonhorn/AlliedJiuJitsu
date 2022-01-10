79bd04bf02669cff51ddd8e08764ce8201332a28
shannon horn <shannonahorn@icloud.com>
2022-01-10 04:15:32 +0000

    instructor updates

--- pages/instructors.js
+++ pages/instructors.js
@@ -1,5 +1,136 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Header from "../controls/header";
+import Footer from "../controls/footer";
+import styles from "../styles/Instructors.module.css";
+import instructorThomas from "../public/images/instructorThomas.jpg";
+import instructorJeff from "../public/images/instructorJeff.jpg";
+import instructorLondon from "../public/images/instructorLondon.jpg";
+
 const Instructors = () => {
-  return <div>instructors</div>;
+  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
+  return (
+    <div className={styles.container}>
+      <a id="top"></a>
+      <main className={styles.main}>
+        <Header
+          hamburgerMenuOpen={hamburgerMenuOpen}
+          setHamburgerMenuOpen={setHamburgerMenuOpen}
+        />
+        <section aria-label="marquee" className={styles.marquee}>
+          <div className={styles.marquee_copy}>
+            <h2
+              className={`${styles.title} ${styles.headline} ${styles.animatePopIn}`}
+            >
+              INSTRUCTORS
+            </h2>
+            <h2
+              className={`${styles.headline} ${styles.headline_sub} ${styles.animatePopIn}`}
+            >
+              Meet the team!
+            </h2>
+          </div>
+        </section>
+        <section
+          aria-label="instructors"
+          className={`${styles.instructors} ${styles.animatePopIn} flow`}
+        >
+          <svg
+            className="icon"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M5.78168 19.25H13.2183C13.7828 19.25 14.227 18.7817 14.1145 18.2285C13.804 16.7012 12.7897 14 9.5 14C6.21031 14 5.19605 16.7012 4.88549 18.2285C4.773 18.7817 5.21718 19.25 5.78168 19.25Z"></path>
+            <path d="M15.75 14C17.8288 14 18.6802 16.1479 19.0239 17.696C19.2095 18.532 18.5333 19.25 17.6769 19.25H16.75"></path>
+            <circle cx="9.5" cy="7.5" r="2.75"></circle>
+            <path d="M14.75 10.25C16.2688 10.25 17.25 9.01878 17.25 7.5C17.25 5.98122 16.2688 4.75 14.75 4.75"></path>
+          </svg>
+          <h3 className="headline">OUR TEAM</h3>
+          <section className={`${styles.instructors_grid} flow`}>
+            <article>
+              <Image
+                src={instructorThomas}
+                alt="Professor Thomas Reichenberger."
+                objectFit="cover"
+                objectPosition="center top"
+                layout="intrinsic"
+                height="3375"
+                width="6000"
+              />
+              <h4 className="headline_sub title">Thomas Reichenberger</h4>
+              <ul>
+                <li>
+                  First degree Brazilian Jiu Jitsu black belt under{" "}
+                  <Link href="https://aresbjjgoodyear.com/Instructors/Professor-Osvaldo-Queixinho-Moizinho">
+                    <a>Professor Osvaldo Queixinho Moizinho (&quot;Q&quot;)</a>
+                  </Link>
+                </li>
+                <li>Began his jiu jitsu journey in 2009</li>
+                <li>CPR, first aid, AED certified</li>
+                <li>Fingerprint clearance</li>
+                <li>Licensed to teach in childcare facilities</li>
+              </ul>
+            </article>
+            <article>
+              <Image
+                src={instructorJeff}
+                alt="Coach Jeff Cook."
+                objectFit="cover"
+                objectPosition="center top"
+                layout="intrinsic"
+                height="3375"
+                width="6000"
+              />
+              <h4 className="headline_sub title">Jeff Cook</h4>
+              <ul>
+                <li>
+                  Brazilian Jiu Jitsu black belt under{" "}
+                  <Link href="https://www.bjjheroes.com/bjj-fighters/john-crouch">
+                    <a>Professor John Crouch</a>
+                  </Link>
+                </li>
+                <li>
+                  Certified Special Operations Combatives Instructor (green
+                  beret)
+                </li>
+                <li>Lead Instructor of Allied Tactical seminars (Gun Jitsu)</li>
+              </ul>
+            </article>
+            <article>
+              <Image
+                src={instructorLondon}
+                alt="Coach London Horn."
+                objectFit="cover"
+                objectPosition="center top"
+                layout="intrinsic"
+                height="3375"
+                width="6000"
+              />
+              <h4 className="headline_sub title">London Horn</h4>
+              <ul>
+                <li>Brazilian Jiu Jitsu purple belt</li>
+                <li>Extremely active jiu jitsu competitor</li>
+                <li>IBJJF 2021 Worlds podium</li>
+                <li>Licensed EMT and CPR certified</li>
+                <li>Competitive power lifter</li>
+              </ul>
+            </article>
+            <article>Shadrack Miller</article>
+            <article>Dave Wilson</article>
+            <article>Tim Metcalf</article>
+            <article>Tyler Metcalf</article>
+            <article>Chris Malicdem</article>
+            <article>Taylor Mohr</article>
+            <article>Josh Dayberry</article>
+            <article>Mike Grandinetti</article>
+            <article>Josh Beno</article>
+          </section>
+        </section>
+        <Footer />
+      </main>
+    </div>
+  );
 };
 
 Instructors.title = "Meet the Allied team";
