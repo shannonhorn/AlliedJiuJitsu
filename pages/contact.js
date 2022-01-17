2df8e98dd29c1bf63e42aeb146d9f32c78fd8cd6
shannon horn <shannonahorn@icloud.com>
2022-01-17 21:02:58 +0000

    contact updates

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import { useForm, ValidationError } from "@formspree/react";
 import Header from "../controls/header";
 import Footer from "../controls/footer";
@@ -37,7 +38,8 @@ const Contact = () => {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M8.89286 4.75H6.06818C5.34017 4.75 4.75 5.34017 4.75 6.06818C4.75 13.3483 10.6517 19.25 17.9318 19.25C18.6598 19.25 19.25 18.6598 19.25 17.9318V15.1071L16.1429 13.0357L14.5317 14.6468C14.2519 14.9267 13.8337 15.0137 13.4821 14.8321C12.8858 14.524 11.9181 13.9452 10.9643 13.0357C9.98768 12.1045 9.41548 11.1011 9.12829 10.494C8.96734 10.1537 9.06052 9.76091 9.32669 9.49474L10.9643 7.85714L8.89286 4.75Z"></path>
+            <circle cx="12" cy="12" r="3.25"></circle>
+            <path d="M12 19.25C7.99594 19.25 4.75 16.0041 4.75 12C4.75 7.99594 7.99594 4.75 12 4.75C18.8125 4.75 19.25 9.125 19.25 12V13.25C19.25 14.3546 18.3546 15.25 17.25 15.25V15.25C16.1454 15.25 15.25 14.3546 15.25 13.25V8.75"></path>
           </svg>
           <h3 className="headline">GET IN TOUCH</h3>
           <form
@@ -77,7 +79,7 @@ const Contact = () => {
               type="phone"
               id="phone"
               name="phone"
-              placeholder="(623) 123-4567"
+              placeholder="(623) 123-4567*"
               title="Provide a valid phone number."
             />
             <ValidationError
@@ -109,6 +111,58 @@ const Contact = () => {
               Thank you! We&apos;ll be in touch.
             </div>
           </form>
+          <section className={styles.contact_below}>
+            <div className={styles.contact_below_block}>
+              <div className={styles.contact_below_icon}>
+                <svg
+                  className="icon"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M8.89286 4.75H6.06818C5.34017 4.75 4.75 5.34017 4.75 6.06818C4.75 13.3483 10.6517 19.25 17.9318 19.25C18.6598 19.25 19.25 18.6598 19.25 17.9318V15.1071L16.1429 13.0357L14.5317 14.6468C14.2519 14.9267 13.8337 15.0137 13.4821 14.8321C12.8858 14.524 11.9181 13.9452 10.9643 13.0357C9.98768 12.1045 9.41548 11.1011 9.12829 10.494C8.96734 10.1537 9.06052 9.76091 9.32669 9.49474L10.9643 7.85714L8.89286 4.75Z"></path>
+                </svg>
+              </div>
+              <div
+                className={`${styles.headline} ${styles.headline_sub} ${styles.title} ${styles.animatePopIn}`}
+              >
+                Say Hello
+              </div>
+              <div>
+                <Link href="mailto:shannon@alliedjiujitsu.com?subject=Reaching out through the website">
+                  <a>Email: shannon@alliedjiujitsu.com</a>
+                </Link>
+              </div>
+              <div>
+                <Link href="tel:6027033067">
+                  <a>Phone: (602) 703-3067</a>
+                </Link>
+              </div>
+            </div>
+            <div className={styles.contact_below_block}>
+              <div className={styles.contact_below_icon}>
+                <svg
+                  className="icon"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M18.25 11C18.25 15 12 19.25 12 19.25C12 19.25 5.75 15 5.75 11C5.75 7.5 8.68629 4.75 12 4.75C15.3137 4.75 18.25 7.5 18.25 11Z"></path>
+                  <circle cx="12" cy="11" r="2.25"></circle>
+                </svg>
+              </div>
+              <div
+                className={`${styles.headline} ${styles.headline_sub} ${styles.title} ${styles.animatePopIn}`}
+              >
+                Where To Meet
+              </div>
+              <Link href="https://g.page/r/CfrXh7y4tklzEAo">
+                <a target="_blank">
+                  <div>10100 West Lake Pleasant Parkway</div>
+                  <div>Suite 1300</div>
+                  <div>Peoria, AZ. 85383</div>
+                </a>
+              </Link>
+            </div>
+          </section>
         </section>
         <Footer />
       </main>
