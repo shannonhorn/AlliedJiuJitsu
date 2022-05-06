e2d17e59de2161a4f0bc0d3f894c832eeaa0ed62
shannon horn <shannonahorn@icloud.com>
2022-05-06 08:25:49 +0000

    updated script

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,33 @@
-import Head from "next/head";
-import "../styles/globals.css";
+import Head from 'next/head';
+import Script from 'next/script';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>
-          {Component.title ?? "Allied Jiu Jitsu in Peoria and Cottonwood AZ"}
+          {Component.title ?? 'Allied Jiu Jitsu in Peoria and Cottonwood AZ'}
         </title>
-        <script
-          defer
-          data-domain="alliedjiujitsu.com"
-          src="https://plausible.io/js/plausible.js"
-        ></script>
+        <Script
+          data-domain='alliedjiujitsu.com'
+          src='https://plausible.io/js/plausible.js'
+        />
         <meta
-          name="description"
+          name='description'
           content={
             Component.description ??
-            "Allied Jiu Jitsu is a premier Brazilian Jiu Jitsu academy with locations in Peoria and Cottonwood, Arizona. We also offer fitness and striking classes for both adults and youth and concealed carry tactical self-defense seminars. Call us today or drop by to check things out FREE!"
+            'Allied Jiu Jitsu is a premier Brazilian Jiu Jitsu academy with locations in Peoria and Cottonwood, Arizona. We also offer fitness and striking classes for both adults and youth and concealed carry tactical self-defense seminars. Call us today or drop by to check things out FREE!'
           }
         />
         <meta
-          name="keywords"
-          content="Brazilian Jiu Jitsu, Jiu Jitsu Near Me, Muay Thai, Striking, Brazilian Jiu Jitsu tournament, Brazilian Jiu Jitsu competition, tactical jiu jitsu, tactical self defense, concealed carry self defense, allied tactical, allied functional fitness, af2, allied yoga, free jiu jitsu for vets, free jiu jitsu for military, free jiu jitsu for cops, free jiu jitsu for police, adopt-a-cop, adopt a cop, free jiu jitsu for firefighters, free jiu jitsu for first responders, 85383, 85382, jiu jitsu for women, jiu jitsu for girls, woman-only jiu jitsu"
+          name='keywords'
+          content='Brazilian Jiu Jitsu, Jiu Jitsu Near Me, Muay Thai, Striking, Brazilian Jiu Jitsu tournament, Brazilian Jiu Jitsu competition, tactical jiu jitsu, tactical self defense, concealed carry self defense, allied tactical, allied functional fitness, af2, allied yoga, free jiu jitsu for vets, free jiu jitsu for military, free jiu jitsu for cops, free jiu jitsu for police, adopt-a-cop, adopt a cop, free jiu jitsu for firefighters, free jiu jitsu for first responders, 85383, 85382, jiu jitsu for women, jiu jitsu for girls, woman-only jiu jitsu'
         ></meta>
-        <meta name="robots" content="index, follow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="UTF-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='robots' content='index, follow' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta charSet='UTF-8' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <Component {...pageProps} />
     </>
