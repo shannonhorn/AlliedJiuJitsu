57bd20ff2a6923a229b38c129edd7552a71bbdd1
shannon horn <shannonahorn@icloud.com>
2022-08-16 18:51:44 +0000

    redirecting...

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta httpEquiv='refresh' content="0; url='https://www.mjja.academy'" />
         <title>
           {Component.title ?? 'Allied Jiu Jitsu in Peoria and Cottonwood AZ'}
         </title>
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class TheDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <meta
+            httpEquiv='refresh'
+            content="0; url='https://www.mjja.academy'"
+          />
           <link
-            href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;600&family=Open+Sans&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Dosis:wght@400;600&family=Open+Sans&display=swap'
+            rel='stylesheet'
           ></link>
         </Head>
         <body>
